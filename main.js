a09c0f96fe1bf0a177c1a70a75f6639169ce4856
emanuele2377 <emanuelekushi7@gmail.com>
2023-11-06 22:19:06 +0100

    .

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const inserisciParola = prompt("Inserisci parola");  
-let reversed = "inserisciParola";
 
-    for (let i = inserisciParola.length() - 1; i >= 0 ; i-- ) 
-      
-      reversed += inserisciParola.charAt(i);
+let reversed = "";
+
+for (let i =  inserisciParola.length -1; i >= 0; i--) {
+  reversed += inserisciParola[i];
+  
+}
+let i =  inserisciParola.length
+  if ( inserisciParola == reversed) {
     
-    return inserisciParola.equals("inserisciParola");
-
+    console.log("palindromo");
+  }
+   else {
+    console.log("non palindromo")
+   }
     
      
