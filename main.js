ffa8cb497f3ac8d8f25acf0390c62d890ec4a613
emanuele2377 <emanuelekushi7@gmail.com>
2023-11-06 16:27:49 +0100

    parole palindromi

--- main.js
+++ main.js
@@ -4,38 +4,29 @@ const inserisciParola = prompt("Inserisci parola");
 
 let autorizzato = false;
 
-// alternativa al ciclo for
-// autorizzato = listaUtenti.includes(mailUtente);
 
 for (let i = 0; i < listaParole.length; i++) {
-    
+
     const element = listaParole[i];
 
-    // console.log( element, i );
-    
-    if(element == inserisciParola) {
+    if (element == inserisciParola) {
         autorizzato = true;
         break;
     }
-    
-    // console.log(autorizzato, i);
+
 }
 
 let parola = "";
 for (let i = 0; i < inserisciParola.length; i++) {
     const carattere = inserisciParola[i];
 
-    if(carattere=="@") {
+    if (carattere == "@") {
         break;
     }
     parola += carattere;
 }
 
-//Alternativa con indexOf e substring
-// const posizioneChiocciola = mailUtente.indexOf("@");
-// nomeUtente = mailUtente.substring(0, posizioneChiocciola);
-
-if(autorizzato) {
+if (autorizzato) {
     alert("e palindroma, " + inserisciParola);
 } else {
     alert("Non e palindroma");
